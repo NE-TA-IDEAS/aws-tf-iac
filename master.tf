f2da8b792e135ce5ceee1068628da6e069911d5e
vijayshmane <46953923+vijayshmane@users.noreply.github.com>
2021-03-31 22:20:45 -0400

    Update master.tf

--- master.tf
+++ master.tf
@@ -1,8 +1,19 @@
-#module "backend-S3" {
-#    source                  = "./modules/s3/backend-s3"
-#}
+module "backend-S3" {
+    source                  = "./modules/s3/backend-s3"
+}
 
 #VPC module
 module "poc-vpc" {
     source                  = "./modules/VPC/POC-VPC"
+    region                  = var.region
+    availabilityZone        = var.availabilityZone
+    instanceTenancy         = var.instanceTenancy
+    dnsSupport              = var.dnsSupport
+    dnsHostNames            = var.dnsHostNames
+    vpcCIDRblock            = var.vpcCIDRblock
+    subnetCIDRblock         = var.subnetCIDRblock
+    destinationCIDRblock    = var.destinationCIDRblock
+    ingressCIDRblock        = var.ingressCIDRblock
+    egressCIDRblock         = var.egressCIDRblock
+    mapPublicIP             = var.mapPublicIP
 }

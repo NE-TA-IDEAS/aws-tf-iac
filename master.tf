f58d1fcec6b997e5290132410d4f3f136e53a09a
vijayshmane <46953923+vijayshmane@users.noreply.github.com>
2021-03-31 21:58:37 -0400

    Update master.tf

--- master.tf
+++ master.tf
@@ -1,6 +1,6 @@
-module "backend-S3" {
-    source                  = "./modules/s3/backend-s3"
-}
+#module "backend-S3" {
+#    source                  = "./modules/s3/backend-s3"
+#}
 
 #VPC module
 module "poc-vpc" {

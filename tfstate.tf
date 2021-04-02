1ccf83b3362f8390f62ad4f934c8eaf9ac89dded
vijayshmane <46953923+vijayshmane@users.noreply.github.com>
2021-04-02 15:26:24 -0400

    Update tfstate.tf

--- tfstate.tf
+++ tfstate.tf
@@ -3,7 +3,5 @@ terraform {
     bucket = "poc-tfstate-files"
     key    = "master.tfstate"
     region = "us-east-2"
-#    access_key = "AKIAU5EHRDAIIOPDWXFZ"
-#    secret_key = "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"
   }
 }

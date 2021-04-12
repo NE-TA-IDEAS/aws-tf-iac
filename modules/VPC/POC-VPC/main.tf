d3f4d86b6da6f7c6eaef72b90192e1b5095d2086
vijayshmane <46953923+vijayshmane@users.noreply.github.com>
2021-04-12 01:06:32 -0400

    Update main.tf

--- modules/VPC/POC-VPC/main.tf
+++ modules/VPC/POC-VPC/main.tf
@@ -1,45 +1,72 @@
-# vpc.tf 
+ vpc.tf
 # Create VPC/Subnet/Security Group/Network ACL
 # create the VPC
 
 resource "aws_vpc" "POC_VPC" {
   cidr_block           = var.vpcCIDRblock
-  instance_tenancy     = var.instanceTenancy 
-  enable_dns_support   = var.dnsSupport 
+  instance_tenancy     = var.instanceTenancy
+  enable_dns_support   = var.dnsSupport
   enable_dns_hostnames = var.dnsHostNames
 tags = {
     Name = "POC VPC"
 }
 } # end resource
 # create the Subnet
-resource "aws_subnet" "POC_VPC_Subnet" {
+resource "aws_subnet" "public_Subnet" {
   vpc_id                  = aws_vpc.POC_VPC.id
-  cidr_block              = var.subnetCIDRblock
-  map_public_ip_on_launch = var.mapPublicIP 
+  cidr_block              = var.publicCIDRblock
+  map_public_ip_on_launch = var.mapPublicIP
   availability_zone       = var.availabilityZone
 tags = {
-   Name = "POC VPC Subnet"
+   Name = "POC public Subnet"
 }
 } # end resource
+
+# create the Subnet
+resource "aws_subnet" "app_Subnet" {
+  vpc_id                  = aws_vpc.POC_VPC.id
+  cidr_block              = var.appCIDRblock
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "POC app Subnet"
+}
+} # end resource
+# create the Subnet
+resource "aws_subnet" "Data_Subnet" {
+  vpc_id                  = aws_vpc.POC_VPC.id
+  cidr_block              = var.dataCIDRblock
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "POC DATA Subnet"
+}
+} # end resource
+
 # Create the Security Group
 resource "aws_security_group" "POC_VPC_Security_Group" {
   vpc_id       = aws_vpc.POC_VPC.id
   name         = "POC VPC Security Group"
   description  = "POC VPC Security Group"
-  
+
   # allow ingress of port 22
   ingress {
-    cidr_blocks = var.ingressCIDRblock  
+    cidr_blocks = var.ingressCIDRblock
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-  } 
-  
+  }
+
   # allow egress of all ports
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "http"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "https"
     cidr_blocks = ["0.0.0.0/0"]
   }
 tags = {
@@ -50,28 +77,28 @@ tags = {
 # create VPC Network access control list
 resource "aws_network_acl" "POC_VPC_Security_ACL" {
   vpc_id = aws_vpc.POC_VPC.id
-  subnet_ids = [ aws_subnet.POC_VPC_Subnet.id ]
+  subnet_ids = [ aws_subnet.public_Subnet.id ]
 # allow ingress port 22
   ingress {
     protocol   = "tcp"
     rule_no    = 100
     action     = "allow"
-    cidr_block = var.destinationCIDRblock 
+    cidr_block = var.destinationCIDRblock
     from_port  = 22
     to_port    = 22
   }
-  
-  # allow ingress port 80 
+
+  # allow ingress port 80
   ingress {
     protocol   = "tcp"
     rule_no    = 200
     action     = "allow"
-    cidr_block = var.destinationCIDRblock 
+    cidr_block = var.destinationCIDRblock
     from_port  = 80
     to_port    = 80
   }
-  
-  # allow ingress ephemeral ports 
+
+  # allow ingress ephemeral ports
   ingress {
     protocol   = "tcp"
     rule_no    = 300
@@ -80,27 +107,27 @@ resource "aws_network_acl" "POC_VPC_Security_ACL" {
     from_port  = 1024
     to_port    = 65535
   }
-  
-  # allow egress port 22 
+
+  # allow egress port 22
   egress {
     protocol   = "tcp"
     rule_no    = 100
     action     = "allow"
     cidr_block = var.destinationCIDRblock
-    from_port  = 22 
+    from_port  = 22
     to_port    = 22
   }
-  
-  # allow egress port 80 
+
+  # allow egress port 80
   egress {
     protocol   = "tcp"
     rule_no    = 200
     action     = "allow"
     cidr_block = var.destinationCIDRblock
-    from_port  = 80  
-    to_port    = 80 
+    from_port  = 80
+    to_port    = 80
   }
- 
+
   # allow egress ephemeral ports
   egress {
     protocol   = "tcp"
@@ -136,8 +163,9 @@ resource "aws_route" "POC_VPC_internet_access" {
 } # end resource
 # Associate the Route Table with the Subnet
 resource "aws_route_table_association" "POC_VPC_association" {
-  subnet_id      = aws_subnet.POC_VPC_Subnet.id
+  subnet_id      = aws_subnet.public_Subnet.id
   route_table_id = aws_route_table.POC_VPC_route_table.id
-} 
+}
+
 # end resource
 # end vpc.tf

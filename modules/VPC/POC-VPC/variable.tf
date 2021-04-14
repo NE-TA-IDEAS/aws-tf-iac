bbf2d4e64dd5c3c3d3407c87d22fd55388f42450
vijayshmane <46953923+vijayshmane@users.noreply.github.com>
2021-04-14 17:05:58 -0400

    Update variable.tf

--- modules/VPC/POC-VPC/variable.tf
+++ modules/VPC/POC-VPC/variable.tf
@@ -21,14 +21,26 @@ variable "dnsHostNames" {
 variable "vpcCIDRblock" {
     default = "10.0.0.0/16"
 }
-variable "publicCIDRblock" {
+variable "publicCIDRblock1" {
     default = "10.0.1.0/24"
 }
-variable "appCIDRblock" {
+
+variable "publicCIDRblock2" {
     default = "10.0.2.0/24"
 }
-variable "dataCIDRblock" {
+variable "appCIDRblock1" {
     default = "10.0.3.0/24"
+}
+
+variable "appCIDRblock2" {
+    default = "10.0.4.0/24"
+}
+variable "dataCIDRblock1" {
+    default = "10.0.5.0/24"
+}
+
+variable "dataCIDRblock2" {
+    default = "10.0.6.0/24"
 }
 variable "destinationCIDRblock" {
     default = "0.0.0.0/0"

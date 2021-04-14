9caaee529badf132790600a5e755e023b6f712c1
vijayshmane <46953923+vijayshmane@users.noreply.github.com>
2021-04-14 16:39:34 -0400

    Update main.tf

--- modules/VPC/POC-VPC/main.tf
+++ modules/VPC/POC-VPC/main.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "public_Subnet" {
   vpc_id                  = aws_vpc.POC_VPC.id
   cidr_block              = var.publicCIDRblock
   map_public_ip_on_launch = var.mapPublicIP
-  availability_zone       = var.availabilityZone
+#  availability_zone       = var.availabilityZone
 tags = {
    Name = "POC public Subnet"
 }
@@ -26,7 +26,7 @@ tags = {
 resource "aws_subnet" "app_Subnet" {
   vpc_id                  = aws_vpc.POC_VPC.id
   cidr_block              = var.appCIDRblock
-  availability_zone       = var.availabilityZone
+#  availability_zone       = var.availabilityZone
 tags = {
    Name = "POC app Subnet"
 }
@@ -35,7 +35,7 @@ tags = {
 resource "aws_subnet" "Data_Subnet" {
   vpc_id                  = aws_vpc.POC_VPC.id
   cidr_block              = var.dataCIDRblock
-  availability_zone       = var.availabilityZone
+#  availability_zone       = var.availabilityZone
 tags = {
    Name = "POC DATA Subnet"
 }
